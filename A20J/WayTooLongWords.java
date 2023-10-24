6ade7341f06b15b11777b16564ce2c6792d8cc77
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-10-24 12:50:33 +0530

    Added WayTooLongWords

---
+++ A20J/WayTooLongWords.java
@@ -0,0 +1,66 @@
+/*
+ * Sometimes some words like "localization" or "internationalization" are so
+ * long that writing them many times in one text is quite tiresome.
+ * 
+ * Let's consider a word too long, if its length is strictly more than 10
+ * characters. All too long words should be replaced with a special
+ * abbreviation.
+ * 
+ * This abbreviation is made like this: we write down the first and the last
+ * letter of a word and between them we write the number of letters between the
+ * first and the last letters. That number is in decimal system and doesn't
+ * contain any leading zeroes.
+ * 
+ * Thus, "localization" will be spelt as "l10n", and
+ * "internationalization» will be spelt as "i18n".
+ * 
+ * You are suggested to automatize the process of changing the words with
+ * abbreviations. At that all too long words should be replaced by the
+ * abbreviation and the words that are not too long should not undergo any
+ * changes.
+ * 
+ * Input
+ * The first line contains an integer n (1 ≤ n ≤ 100). Each of the following n
+ * lines contains one word. All the words consist of lowercase Latin letters and
+ * possess the lengths of from 1 to 100 characters.
+ * 
+ * Output
+ * Print n lines. The i-th line should contain the result of replacing of the
+ * i-th word from the input data.
+ */
+
+import java.util.Scanner;
+
+public class WayTooLongWords {
+    public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
+
+        int n = io.nextInt();
+
+        String[] result = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder s = new StringBuilder(io.next());
+
+            int len = s.length();
+
+            StringBuilder abbrevation = new StringBuilder();
+
+            if (len > 10) {
+                abbrevation.append(s.charAt(0));
+                abbrevation.append(len - 2);
+                abbrevation.append(s.charAt(len - 1));
+            } else {
+                abbrevation.append(s);
+            }
+
+            result[i] = abbrevation.toString();
+        }
+
+        for (int j = 0; j < result.length; j++) {
+            System.out.println(result[j]);
+        }
+
+        io.close();
+    }
+}
